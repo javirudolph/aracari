6f2609ceab073a92cbb565ae08b4103e297e6601
Javi Rudolph <javiera.av@ufl.edu>
2021-08-18 17:35:45 -0400

    Added functions

--- Ch1_movement_rates/Ch1_theo_sims.R
+++ Ch1_movement_rates/Ch1_theo_sims.R
@@ -104,3 +104,48 @@ for(i in 2:5){
 #add legend
 legend("topright", legend= m_3,
        col=mycols, lty=1, cex=1.2)
+
+# Functions --------------------------------------------------------------------------
+sim_movement <- function(prm, t = 1000, plot.it = TRUE, return.data.frame = FALSE){
+  tru.rate <- round(prm, 3)
+  movedist <- rexp(t, rate = 1/tru.rate)
+  angle <- runif(t, min = 0, max = 360)
+  distx <- movedist*cos(angle)
+  xloc <- c(0, cumsum(distx))
+  disty <- movedist*sin(angle)
+  yloc <- c(0, cumsum(disty))
+  animalTraj <- data.frame(time = 0:t, xloc = xloc, yloc = yloc)
+  if(plot.it == TRUE){
+    plot(x = xloc, y = yloc, type = "l", main = paste("Rate=",tru.rate))
+  }
+  if(return.data.frame == TRUE){
+    return(animalTraj)
+  }
+}
+
+sim_seeds <- function(nseeds = 20, m.prms = NULL,...){
+  grt <- round(rgamma(nseeds, shape = 4, scale = 5))
+  t.grt <- max(grt)
+
+  df <- sim_movement(m.prms, t = t.grt, plot.it = FALSE, return.data.frame = TRUE)
+
+  df %>%
+    left_join(., data.frame(s.id = 1:nseeds, time = grt), by = "time") %>%
+    mutate(disp = sqrt(xloc^2+yloc^2)) -> df
+
+  return(df)
+}
+
+summ_seeds <- function(df = NULL){
+  df %>%
+    drop_na(s.id) %>%
+    mutate(xi = (mean(xloc)-xloc)^2,
+           yi = (mean(yloc)-yloc)^2) %>%
+    summarise(x = mean(xloc),
+              y = mean(yloc),
+              av.disp = mean(disp),
+              se.disp = sd(disp)/sqrt(n()),
+              dsprsn = sum(sqrt(xi+yi))/n()) -> s.df
+  return(s.df)
+}
+
