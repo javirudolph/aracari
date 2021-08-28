26bf6e66260be6ee68f7241ab264cb0b0d29d64e
Javi Rudolph <javiera.av@ufl.edu>
2021-08-28 08:23:56 -0400

    Generate script for simulating movement using aracari movement rates

---
+++ Ch1_movement_rates/Ch1_data_sims.R
@@ -0,0 +1,12 @@
+# Script to run simulations for theoretical populations with three levels of variation in movement rates
+# LIBRARIES --------------------------------------------------------------------------------
+
+
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(cowplot)
+library(fitdistrplus)
+library(Hmisc)
+
+set.seed(98)
