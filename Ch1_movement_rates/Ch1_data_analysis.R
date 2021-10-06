9a430d5676d16f0f0a92305796e319b6d61963ed
JaviDesk <f.javierarudolph@gmail.com>
2021-10-06 17:16:36 -0400

    Small edits

--- Ch1_movement_rates/Ch1_data_analysis.R
+++ Ch1_movement_rates/Ch1_data_analysis.R
@@ -17,8 +17,9 @@ load("Ch1_movement_rates/sims_backup/datagen_pp.RData")
 load("Ch1_movement_rates/sims_backup/datagen_ppi.RData")
 load("Ch1_movement_rates/sims_backup/datagen_np.RData")
 
-# CP Kernel ------------------------------------------------------------
-# sample with replacement groups of 100 seeds and fit weibull , here are the parameters.
+# Weibull Seed Dispersal Kernels --------------------------------------------------------
+## CP Kernel ------------------------------------------------------------
+# sample with replacement groups of 100 seeds and fit weibull
 n.boots <- 1000
 samp.size <- 100
 weib.boot.cp <- NULL
@@ -40,7 +41,7 @@ for(j in 1:n.boots){
 
 save(weib.boot.cp, file = "Ch1_movement_rates/sims_backup/weib_cp.RData")
 
-# PP Kernel ---------------------------------------------------------------------
+## PP Kernel ---------------------------------------------------------------------
 n.boots <- 1000
 samp.size <- 100
 weib.boot.pp <- NULL
@@ -62,7 +63,50 @@ for(j in 1:n.boots){
 
 save(weib.boot.pp, file = "Ch1_movement_rates/sims_backup/weib_pp.RData")
 
-### NP Kernel ------------------------------------------------------------------
+## PPi Kernel -------------------------------------------------------------------
+
+n.boots <- 1000
+samp.size <- 100
+weib.boot.ppi <- NULL
+
+for(j in 1:n.boots){
+  s.df <- ppi.df %>% drop_na(s.id) %>%
+    group_by(model) %>%
+    sample_n(., samp.size) %>%
+    mutate(disp = round(disp, digits = 2))
+
+  # s.df %>%
+  #   ggplot(., aes(x = disp, fill = popu)) +
+  #   geom_histogram()
+
+  # s.df <- df %>% drop_na(s.id)
+
+  weib.fits <- NULL
+
+  for(i in 1:7){
+    dat <- s.df %>%
+      filter(model == paste0("ppi_", i)) %>%
+      filter(disp != 0)
+
+    g <- fitdist(dat$disp, distr = "weibull", method = 'mle', lower = c(0,0))
+    #g <- fitdistr(dat$disp, densfun = "weibull", lower = c(0,0))
+    prms.weib <- data.frame(est.shape = as.numeric(g$estimate[1]),
+                            est.scale = as.numeric(g$estimate[2]),
+                            loglik = g$loglik,
+                            popu = i,
+                            model = "ppi")
+    weib.fits <- rbind.data.frame(weib.fits, prms.weib)
+    # plot(g)
+  }
+
+  weib.boot.ppi <- rbind.data.frame(weib.boot.ppi, weib.fits %>% mutate(boot = j))
+}
+
+
+save(weib.boot.ppi, file = "Ch1_movement_rates/sims_backup/weib_ppi.RData")
+
+
+## NP Kernel ------------------------------------------------------------------
 n.boots <- 1000
 samp.size <- 100
 weib.boot.np <- NULL
@@ -91,7 +135,8 @@ for(j in 1:n.boots){
     prms.weib <- data.frame(est.shape = as.numeric(g$estimate[1]),
                             est.scale = as.numeric(g$estimate[2]),
                             loglik = g$loglik,
-                            popu = i)
+                            popu = i,
+                            model = paste0("np_", i))
     weib.fits <- rbind.data.frame(weib.fits, prms.weib)
     # plot(g)
   }
@@ -101,3 +146,7 @@ for(j in 1:n.boots){
 
 
 save(weib.boot.np, file = "Ch1_movement_rates/sims_backup/weib_np.RData")
+
+
+# Long distance dispersal summaries ----------------------------------------------------
+# Calculate percentage of LDD seeds, and mean and sd for dispersal and dispersion metrics per run. This to make that table again.
