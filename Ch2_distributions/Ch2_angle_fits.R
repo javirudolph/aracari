815ce59aad6bd4ed5b1cb082289d732da906effc
JaviDesk <f.javierarudolph@gmail.com>
2022-01-10 15:10:14 -0500

    This is on the markdown now.

--- Ch2_distributions/Ch2_angle_fits.R
+++ Ch2_distributions/Ch2_angle_fits.R
@@ -13,25 +13,72 @@ library(CircStats)
 
 # Data ----------------------------------------------------
 # The script for this data is in the data-raw folder
-load("data/ptpl.rda")
+load("Ch2_distributions/Orig_data_KH/tidy_data.RData")
+
+
+ptpl %>%
+  mutate(individual = paste0("B", id))%>%
+  dplyr::select(individual, sgroup, rel.angle, abs.angle) %>%
+  drop_na(individual, rel.angle) -> angles_df
+
 
 # Wrapped Cauchy fit
 
 ?dwrpcauchy
 
-#vector of angles in radians:
-
-ptpl %>%
-  drop_na(rel.angle) -> angle_data
-
 
 # Estimate for complete pooling
 
-mu0 <- circ.mean(angle_data$rel.angle)
-rho0 <- est.rho(angle_data$rel.angle)
-a <- wrpcauchy.ml(angle_data$rel.angle, mu0 = mu0, rho0 = rho0)
+mu0 <- circ.mean(angles_df$rel.angle)
+rho0 <- est.rho(angles_df$rel.angle)
+angle.params <- as.data.frame(wrpcauchy.ml(angles_df$rel.angle, mu0 = mu0, rho0 = rho0)) %>%
+  mutate(model = "CP",
+         group = NA)
 
 
+# Partial pooling
+
+sgroups <- paste0("G", 1:7)
+
+
+
+for(i in 1:7){
+
+  sub_data <- angles_df %>%
+    drop_na(rel.angle) %>%
+    filter(., sgroup == sgroups[i])
+
+  mu0 <- circ.mean(sub_data$rel.angle)
+  rho0 <- est.rho(sub_data$rel.angle)
+  a <- as.data.frame(wrpcauchy.ml(sub_data$rel.angle, mu0 = mu0, rho0 = rho0)) %>%
+    mutate(model = "PP",
+           group = sgroups[i])
+
+  angle.params <- rbind.data.frame(angle.params, a)
+
+}
+
+# No pooling
+
+individuals <- unique(angles_df$individual)
+
+
+
+for(i in 1:15){
+
+  sub_data <- angles_df %>%
+    drop_na(rel.angle) %>%
+    filter(., individual == individuals[i])
+
+  mu0 <- circ.mean(sub_data$rel.angle)
+  rho0 <- est.rho(sub_data$rel.angle)
+  a <- as.data.frame(wrpcauchy.ml(sub_data$rel.angle, mu0 = mu0, rho0 = rho0)) %>%
+    mutate(model = "NP",
+           group = individuals[i])
+
+  angle.params <- rbind.data.frame(angle.params, a)
+
+}
 
 
 
