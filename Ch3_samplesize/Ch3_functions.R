f8be8a253ab3ad98541ddc602ecf70a06095d34d
JaviDesk <f.javierarudolph@gmail.com>
2022-02-18 15:17:44 -0500

    Building the functions script from the Rmd file

--- Ch3_samplesize/Ch3_functions.R
+++ Ch3_samplesize/Ch3_functions.R
@@ -1,92 +1,101 @@
-### Lomax functions --------------------------
+##########################################
+## Javiera Rudolph                      ##
+### Functions used for Ch3              ##
+## February 18, 2022                    ##
+##########################################
 
-lomax.pdf <- function(x,alpha,k, log.scale=FALSE){
 
-  if(log.scale==FALSE){out <- (k/(alpha+x))*(alpha/(alpha+x))^k
-  }else{
-    out <- log(k) + k*log(alpha) - (k+1)*log(alpha+x)
-  }
 
-  return(out)
+##########################################
+# Data simulation ------------------------
+
+
+## Mixture model -------------------------
+
+# The weights right now are just set as values
+# but this can change, and we could use a distribution
+# to sample them from
+
+### Weights ------------------------------
+
+# This is not created yet, but on the list
+
+generate.pis <- functions(n){
+  NULL
 }
 
 
-lomax.cdf <- function(x,alpha,k){
+### Components --------------------------
 
-  return(1-(alpha/(alpha+x))^k)
+#### Parameters -------------------------
+# since I get confused with logs and such
+# The following function helps gives the
+# parameters for a lognormal that has the
+# desired mean and variance:
 
+desired_mean_sd <- function(mu_x, sigsq_x){
+
+  mu <- log(mu_x^2/(sqrt(mu_x^2+sigsq_x)))
+  sigsq <- log(1+(sigsq_x/mu_x^2))
+
+  return(data.frame(mu, sigsq))
 }
 
-ft.nllike <- function(guess=init.betas, designmat=designmat,Y=Y){
+# Now, for a Lnorm(mu, sigsq), get mean and var
 
-  Xbeta         <- designmat%*%guess
-  alphas        <- exp(Xbeta) # because alpha = ln(X*betas)
-  n             <- length(Y)
-  sumlogapy     <- sum(log(alphas+Y))
-  k.hat         <- n/(sumlogapy - sum(Xbeta))
-  lnft.yis     <- lomax.pdf(x=Y, alpha=alphas,k=k.hat,log.scale=TRUE)
-  lnL           <- sum(lnft.yis)
-  nll           <- -lnL
-  return(nll)
+lnorm_mean_var <- function(mu, sigsq){
+
+  lnorm_mean <- exp(mu + (sigsq/2))
+  lnorm_var  <- (exp(sigsq)-1)*exp(2*mu+sigsq)
+
+  return(data.frame(lnorm_mean, lnorm_var))
 }
 
-lomax.glm <- function(formula=~1, my.dataf, response){
 
-  Y           <- response
-  n           <- length(Y)
-  designmat   <- model.matrix(formula, data=my.dataf)
-  nbetas      <- ncol(designmat)
-  init.betas  <- rep(1.5,nbetas)
+#### Components --------------------------
 
-  opt.out <- optim(par=init.betas, fn=ft.nllike, method="BFGS",
-                   designmat=designmat, Y=Y)
+# This is a function to make the multiple curves to plot
+# I always struggle to remember how to make this
 
-  mles          <- opt.out$par
-  nll.hat       <- opt.out$value
-  BIC.mod       <- 2*nll.hat + nbetas*log(length(Y))
-  Xbeta.hat     <- designmat%*%mles
-  alphas.hat    <- exp(Xbeta.hat)
-  sumlogapy.hat <- sum(log(alphas.hat+Y))
-  k.hat         <- n/(sumlogapy.hat - sum(Xbeta.hat))
+lnorm_densities_fx <- function(mu_x, sigsq_x, color_x){
 
-  out.list <- list(opt.out = opt.out, designmat=designmat,Y=Y, mles=mles, nll.hat=nll.hat, BIC.mod = BIC.mod,
-                   alphas.hat=alphas.hat, k.hat=k.hat,data=my.dataf)
+  # I'm assuming we are giving variances, so need to change into sd.
+  sd_x <- sqrt(sigsq_x)
 
-  return(out.list)
-
-}
-# Sample 1000 movements and fit a lomax
-
-my.df <- data.frame(x = sample(all.samples, 1000))
-mod1 <- lomax.glm(formula = ~1, my.dataf = my.df, response = my.df$x)
-
-# What are the estimated parameters?
-alpha <- mod1$alphas.hat[1] # equals exp(mod1$mles)
-k <- mod1$k.hat
-
-# Estimate the Pr(X <= x)
-# Probability that a value drawn from this lomax is smaller or equal to 500
-lomax.cdf(x = 500, alpha = alpha, k = k)
-
-# Probability that it is greater.
-lomax.st <- function(x=x, alpha=alpha, k=k){
-  out <- alpha/(alpha+x)
-  return(out^k)
+  my_curvs <- purrr::map(1:length(mu_x), function(y) stat_function(fun = dlnorm,
+                                                               args = list(meanlog = mu_x[y], sdlog = sd_x[y]),
+                                                               color = color_x[y], size=1))
+  return(my_curvs)
 }
 
-lomax.st(500, alpha, k)
 
 
 
-# calculate the mean of the fit
 
-lomax.mean <- function(alpha=alpha, k=k){
-  return(alpha/(k+1))
-}
 
-lomax.mean(alpha, k)
 
-# Assess the fit using model quantiles to make a qqplot
-lomax.quantile <- function(alpha, k, p){
-  return(alpha*(((1-p)^(1/k))-1))
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
