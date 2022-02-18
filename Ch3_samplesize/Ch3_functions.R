8e32a09d86e2bc750727701be93025c654ac456a
JaviDesk <f.javierarudolph@gmail.com>
2022-02-18 16:01:53 -0500

    Added lomax functions

--- Ch3_samplesize/Ch3_functions.R
+++ Ch3_samplesize/Ch3_functions.R
@@ -71,6 +71,85 @@ lnorm_densities_fx <- function(meanlog_x, sdlog_x, color_x){
 
 
 
+# LOMAX -------------------------------------------
+# All the lomax functions
+
+# FITTING -------------------------------------
+# We use a Lomax distribution, which is actually a special case of generalized Pareto distribution
+# The Lomax or Patro type II has support on x=0, and is a heavy tail distribution
+# Jose's document shows that it arises from incorporating variability in the rate parameter of an exponential distribution, using a gamma distribution - which he calls the hierarchical
+# It only has two parameters, alpha(shape) and lambda(scale)
+
+lomax.pdf <- function(x,alpha,k, log.scale=FALSE){
+
+  if(log.scale==FALSE){out <- (k/(alpha+x))*(alpha/(alpha+x))^k
+  }else{
+    out <- log(k) + k*log(alpha) - (k+1)*log(alpha+x)
+  }
+
+  return(out)
+}
+
+
+lomax.cdf <- function(x,alpha,k){
+
+  return(1-(alpha/(alpha+x))^k)
+
+}
+
+lomax.st <- function(x=x, alpha=alpha, k=k){
+  out <- alpha/(alpha+x)
+  return(out^k)
+}
+
+lomax.mean <- function(alpha=alpha, k=k){
+  return(alpha/(k-1))
+}
+
+
+ft.nllike <- function(guess=init.betas, designmat=designmat,Y=Y){
+
+  nbetasp1      <- length(guess)
+  k             <- exp(guess[1])
+  Xbeta         <- designmat%*%guess[2:nbetasp1]
+  alphas        <- exp(Xbeta) # because alpha = ln(X*betas)
+  n             <- length(Y)
+
+  #sumlogapy     <- sum(log(alphas+Y))
+  #k.hat         <- n/(sumlogapy - sum(Xbeta))
+  lnft.yis     <- lomax.pdf(x=Y, alpha=alphas,k=k,log.scale=TRUE)
+  lnL           <- sum(lnft.yis)
+  nll           <- -lnL
+
+  return(nll)
+}
+
+
+lomax.glm <- function(formula=~1, my.dataf, response){
+
+  Y           <- response
+  n           <- length(Y)
+  designmat   <- model.matrix(formula, data=my.dataf)
+  nbetas      <- ncol(designmat)
+  init.betas  <- c(4,rep(5,nbetas))
+
+  opt.out <- optim(par=init.betas, fn=ft.nllike, method = "Nelder-Mead",
+                   designmat=designmat, Y=Y)
+
+  mles          <- opt.out$par
+  nll.hat       <- opt.out$value
+  BIC.mod       <- 2*nll.hat + nbetas*log(length(Y))
+  Xbeta.hat     <- designmat%*%mles[-1]
+  alphas.hat    <- log(Xbeta.hat)
+  #sumlogapy.hat <- sum(log(alphas.hat+Y))
+  k.hat         <-  mles[1] #n/(sumlogapy.hat - sum(Xbeta.hat))
+
+  out.list <- list(opt.out = opt.out, designmat=designmat,Y=Y, mles=mles, nll.hat=nll.hat, BIC.mod = BIC.mod,
+                   alphas.hat=alphas.hat, k.hat=k.hat,data=my.dataf)
+
+  return(out.list)
+
+}
 
 
 
