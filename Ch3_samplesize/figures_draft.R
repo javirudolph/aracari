212f057036e0728bc38dfb9a19cd8a1945e5fcd0
JaviDesk <f.javierarudolph@gmail.com>
2022-02-24 10:54:19 -0500

    Script dedicated to figures

---
+++ Ch3_samplesize/figures_draft.R
@@ -0,0 +1,24 @@
+## Ch3 figures ##
+
+# Figure 1 -----
+# Describe the different scenarios
+# I vote for four plus the example with the Russo dispersal kernel
+
+# Maybe this is actually a graphic showing each scenario as the change in variance and the mean.
+
+
+# Figure 2 -----
+# Describe the "real" data? The histograms for the 50k samples and the density curve associated to that.
+
+
+# Figure 3 ----
+# The bias of the estimator, for each threshold and across sample size for all scenarios
+
+
+# Figure 4 ----
+# Bias corrected estimator compared to the uncorrected one
+# Does the variance decrease? Does sample size have an effect?
+
+
+# Figure 5 -----
+# Full process for the russso kernel as the example in "real" life
