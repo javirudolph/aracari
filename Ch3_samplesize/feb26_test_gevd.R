29a3057e61c736100d64e04f8cbe0176cdc1b666
JaviDesk <f.javierarudolph@gmail.com>
2022-02-26 20:19:51 -0500

    Different approach. Bootstrap the data to get the median standard deviation of the bootstraps and use that as the threshold.

--- Ch3_samplesize/feb26_test_gevd.R
+++ Ch3_samplesize/feb26_test_gevd.R
@@ -52,7 +52,7 @@ all_test_combos <- data.frame(expand.grid(thresh_tests = thresh_tests, samp_n_te
 
 
 
-# RERUN MLES with threshold -------------------------------------
+# GP STATIC THRESHOLD -------------------------------------
 
 # Load the nreps from the ponciano desktop folder
 # Run nreps with a threshold for GP (nreps is 50)
@@ -87,6 +87,8 @@ for(j in 1:50){
     right_join(., thetas[, c(1,3)]) -> mles_df
   out <- rbind.data.frame(out, mles_df)
 }
+
+gp_static <- out
 
 
 # Compare them -------------------------------
@@ -131,7 +133,7 @@ nreps_mles_df %>%
 
 pleg <- get_legend(f4_evd)
 
-out %>%
+gp_static %>%
   filter(., GP_t != 0) %>%
   ggplot(., aes(y = log(GP_t/theta), group = thresh_tests, color = samp_n_tests)) +
   facet_wrap(~samp_n_tests, nrow = 1) +
@@ -146,66 +148,127 @@ out %>%
 
 plot_grid(f4_lomax, f4_glm, f4_evd + theme(legend.position = "none"), f5_evd, pleg, nrow = 5, rel_heights = c(1,1,1,1, 0.3))
 
-# FIND THE THRESHOLD -------------------
-
-ith_n <- samp_n_tests[5]
-
-ith_samps <- data.frame(x_star = sample(truth_df$x_samps, ith_n))
-mrlplot(ith_samps$x_star)
-
-threshrange.plot(ith_samps$x_star, r = c(0, 500), type = "GP")
-
-fit <- fevd(ith_samps$x_star, threshold = 50, type = "GP")
-plot(fit)
-
-# How do we use these tools from the pareto stuff with the lomax distribution
-# How do we inform the model to correct the bias in the tail estimate?
 
 
-### Multiple runs ---------------------------------------
-nruns <- paste0("nrun_", 1:points_for_boxplots)
-combo_tests <- data.frame(expand.grid(thresh_tests = thresh_tests, samp_n_tests = samp_n_tests, nruns = nruns))
-nonpar_glm <- combo_tests
+# GP 75th THRESH -----------------------------------------------
 
-for(i in 1:nrow(combo_tests)){
-  # Get the sample
-  ith_n <- combo_tests$samp_n_tests[i]
-  ith_thresh <- combo_tests$thresh_tests[i]
-  ith_samps <- data.frame(x_star = sample(truth_df$x_samps, ith_n))
 
-  # Lomax GLM
-  ith_glm <- lomax.glm(formula=~1, my.dataf=ith_samps, response=ith_samps$x_star)
-  ith_alpha_hat <- ith_glm$alphas.hat[1]
-  ith_k_hat <- ith_glm$k.hat
+out <- data.frame()
 
-  # Estimate tail
-  ith_theta_hat <- lomax.St(x = ith_thresh, alpha = ith_alpha_hat, k = ith_k_hat, log.scale=TRUE)
-  nonpar_glm$theta_hat[i] <- ith_theta_hat
+for(j in 1:50){
+  samp_n_tests <- c(80, 200, 500, 800, 1000, 1600)
+  mles_df <- data.frame(expand.grid(thresh_tests = thresh_tests, samp_n_tests = samp_n_tests))
+  mles_df$nrep <- j
 
-  # Nonparametric bootstrapping
-  bth_df <- data.frame(n_B = 1:B)
+  for(i in 1:nrow(mles_df)){
+    ith_n <- mles_df$samp_n_tests[i]
+    ith_thresh <- mles_df$thresh_tests[i]
+    ith_samps <- data.frame(x_star = sample(truth_df$x_samps, ith_n))
+    ith_tail <- length(which(ith_samps$x_star >= ith_thresh))
+    mles_df$tail_p[i] <- ith_tail/ith_n
+    ith_quant <- round(quantile(ith_samps$x_star, 0.75))
 
-  for(b in 1:B){
-    # Sample with replacement from the given sample
-    bth_samps <- data.frame(x_star = sample(ith_samps$x_star, ith_n, replace = TRUE))
 
-    # Estimate params using glm Lomax
-    bth_glm <- lomax.glm(formula=~1, my.dataf=bth_samps, response=bth_samps$x_star)
-    bth_alpha_star <- bth_glm$alphas.hat[1]
-    bth_k_star <- bth_glm$k.hat
+    # Check with GP fit
+    ith_evd <- fevd(ith_samps$x_star, threshold = ith_quant, type = "GP")
+    gp_scale <- summary(ith_evd)$par[1]
+    gp_shape <- summary(ith_evd)$par[2]
 
-    # Estimate the tail
-    bth_df$log_theta_star[b] <- lomax.St(x = ith_thresh, alpha = bth_alpha_star, k = bth_k_star, log.scale=TRUE)
-    bth_df$theta_star[b] <- lomax.St(x = ith_thresh, alpha = bth_alpha_star, k = bth_k_star, log.scale=FALSE)
-
+    # GP tail
+    # mles_df$log_GP[i] <- log(pextRemes(ith_evd, ith_thresh, lower.tail = FALSE))
+    mles_df$GP_t[i] <- pextRemes(ith_evd, ith_thresh, lower.tail = FALSE)
   }
 
-  nonpar_glm$log_theta_stars[i] <- mean(bth_df$log_theta_star)
-  nonpar_glm$theta_stars[i] <- mean(bth_df$theta_star)
-
+  mles_df %>%
+    right_join(., thetas[, c(1,3)]) -> mles_df
+  out <- rbind.data.frame(out, mles_df)
 }
 
-nonpar_glm %>%
-  mutate(theta_bar = 2*theta_hat - log_theta_stars) %>%
-  right_join(., thetas[, c(1,3)]) %>%
-  mutate(theta = log(theta)) -> nonpar_glm
+gp_75th <- out
+
+
+gp_75th %>%
+  filter(., GP_t != 0) %>%
+  ggplot(., aes(y = log(GP_t/theta), group = thresh_tests, color = samp_n_tests)) +
+  facet_wrap(~samp_n_tests, nrow = 1) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_hline(aes(yintercept=  0), color = palette4_sampsize[1]) +
+  scale_color_gradient(low = palette4_sampsize[2], high = palette4_sampsize[3]) +
+  labs(x = "Threshold Value", y = "Log GP - Log theta") +
+  scale_x_continuous(breaks = thresh_tests) +
+  theme_bw() +
+  theme(legend.position = "none")  -> f5_75th
+
+
+
+
+
+
+
+# Can you do a nonparametric bootstrap of the quantile to define the threshold?
+
+out <- data.frame()
+
+for(j in 1:50){
+  samp_n_tests <- c(80, 200, 500, 800, 1000, 1600)
+  mles_df <- data.frame(expand.grid(thresh_tests = thresh_tests, samp_n_tests = samp_n_tests))
+  mles_df$nrep <- j
+
+  for(i in 1:nrow(mles_df)){
+    ith_n <- mles_df$samp_n_tests[i]
+    ith_thresh <- mles_df$thresh_tests[i]
+    ith_samps <- data.frame(x_star = sample(truth_df$x_samps, ith_n))
+
+
+    # nonparam bootstrap to define the threshold
+    k_out <- NULL
+    for(k in 1:1000){
+      kth_samps <- data.frame(x_star = sample(ith_samps$x_star, ith_n, replace = TRUE))
+      k_quantiles <- round(as.numeric(quantile(kth_samps$x_star, 0.3)))
+      k_sd <- sd(kth_samps$x_star)
+
+      k_out <- rbind(k_out, k_sd)
+
+    }
+    kth_thresh <- median(k_out)
+
+    ith_tail <- length(which(ith_samps$x_star >= ith_thresh))
+    mles_df$tail_p[i] <- ith_tail/ith_n
+    ith_quant <- round(as.numeric(quantile(ith_samps$x_star, 0.5)))
+
+
+    # Check with GP fit
+    ith_evd <- fevd(ith_samps$x_star, threshold = kth_thresh, type = "GP")
+    gp_scale <- summary(ith_evd)$par[1]
+    gp_shape <- summary(ith_evd)$par[2]
+
+    # GP tail
+    # mles_df$log_GP[i] <- log(pextRemes(ith_evd, ith_thresh, lower.tail = FALSE))
+    mles_df$GP_t[i] <- pextRemes(ith_evd, ith_thresh, lower.tail = FALSE)
+    mles_df$kth_thresh[i] <- kth_thresh
+  }
+
+  mles_df %>%
+    right_join(., thetas[, c(1,3)]) -> mles_df
+  out <- rbind.data.frame(out, mles_df)
+}
+
+gp_sd_thresh <- out
+
+
+gp_sd_thresh %>%
+  %>%
+  filter(., GP_t != 0) %>%
+  ggplot(., aes(y = log(GP_t/theta), group = thresh_tests, color = samp_n_tests)) +
+  facet_wrap(~samp_n_tests, nrow = 1) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_hline(aes(yintercept=  0), color = palette4_sampsize[1]) +
+  scale_color_gradient(low = palette4_sampsize[2], high = palette4_sampsize[3]) +
+  labs(x = "Threshold Value", y = "Log GP - Log theta") +
+  scale_x_continuous(breaks = thresh_tests) +
+  theme_bw() +
+  theme(legend.position = "none") -> f5_sd_thresh
+
+
+
+
